265e843fae8b533d3adbc9b43b4ff495a592cf33
Usuario <Usuario@CPD03-M20>
2024-11-04 15:09:54 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -0,0 +1,35 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppMinhasCompras.Models
+{
+    public class Produto
+    {
+        string descricao;
+        [PrimaryKey, AutoIncrement]
+
+        public string Descricao
+        {
+            get => descricao;
+            set
+            {
+                if (value == null)
+                {
+                    throw new Exception("Por favor, preencha a descrição");
+                }
+                descricao = value;
+            }
+
+        }
+    }
+    public double Quantidade { get; set; }
+    public double Preco { get; set; } 
+    public double Total { get => Quantidade * Preco; }
+
+
+
+}
